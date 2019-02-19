a17cbb40ac950a23d1ebd0726de4f498270f1538
Stefanie Wehrle <steffinchen@gmail.com>
2019-02-19 17:13:26 +0000

    :sparkles: schema changes for drawing cards

--- src/server/schema.js
+++ src/server/schema.js
@@ -14,12 +14,30 @@ const schema = buildASTSchema(gql`
     remaining: Int!
     success: Boolean!
     shuffled: Boolean!
+    cards: [Card]
+  }
+
+  type Card {
+    image: String
+    value: String
+    suit: String
+    code: String
+  }
+
+  type Mutation {
+    drawCard(deck_id: String!): Deck!
   }
 `);
 
 const root = {
   deck: async () => {
     const data = await fetch(BASE_URI + "deck/new/shuffle", {
+      method: "GET"
+    });
+    return await data.json();
+  },
+  drawCard: async ({ deck_id }) => {
+    const data = await fetch(BASE_URI + `deck/${deck_id}/draw/?count=2`, {
       method: "GET"
     });
     return await data.json();
