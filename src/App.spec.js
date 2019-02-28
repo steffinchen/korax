d2b87430230d35d96f04c4179ba7738fcc6eb3e4
Stefanie Wehrle <steffinchen@gmail.com>
2019-02-28 17:11:31 +0000

    :white_check_mark: have a simple, passing test
    With enzyme and jest

---
+++ src/App.spec.js
@@ -0,0 +1,13 @@
+import React from "react";
+import enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import App from "./App";
+import GraphqlData from "./graphql-data";
+
+enzyme.configure({ adapter: new Adapter() });
+
+it("renders without crashing", () => {
+  const app = shallow(<App />);
+  expect(app.find(GraphqlData)).toBeTruthy();
+});
